f94bdc7ead16252f61fcf9a2eea3239a2b3ee233
Krichanov Sergey <dsirax@icloud.com>
2023-12-13 15:21:14 +0100

    fix requirements

--- conftest.py
+++ conftest.py
@@ -1,11 +1,37 @@
 import pytest
-from selene.support.shared import browser, config
 
-@pytest.fixture(scope="function")
-def start_settings_google():
-    config.window_width = 1920
-    config.window_height = 1080
-    browser.config.base_url = 'https://demoqa.com'
-    yield
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+from utils import attach
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+
     browser.clear_local_storage()
+    browser.quit()
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
     browser.quit()
